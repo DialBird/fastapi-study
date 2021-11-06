0ac604bd779d1e68aafd80623ee7e068a8c8f35b
Keisuke Taniguchi <rememberthatk@icloud.com>
2021-11-06 13:56:09 +0900

    add user

--- app/main.py
+++ app/main.py
@@ -1,9 +1,11 @@
+from typing import List
 from fastapi import FastAPI, Response, status, HTTPException, Depends
 from pydantic import BaseModel
 import psycopg2
 from psycopg2.extras import RealDictCursor
-from sqlalchemy.orm import session
-from . import models
+from sqlalchemy.orm import Session
+
+from . import models, schemas, utils
 from .database import engine, SessionLocal
 
 models.Base.metadata.create_all(bind=engine)
@@ -17,12 +19,6 @@ def get_db():
         yield db
     finally:
         db.close()
-
-
-class Post(BaseModel):
-    title: str
-    content: str
-    published: bool = True
 
 
 try:
@@ -50,73 +46,65 @@ def root():
     return {"message": "Hello World"}
 
 
-@app.get("/sqlalchemy")
-def test_posts(db: session = Depends(get_db)):
+@app.get("/posts", response_model=List[schemas.Post])
+def get_posts(db: Session = Depends(get_db)):
     posts = db.query(models.Post).all()
-    return {"data": posts}
+    return posts
 
 
-@app.get("/posts")
-def get_posts():
-    cursor.execute("""SELECT * FROM posts""")
-    posts = cursor.fetchall()
-    return {"data": posts}
+@app.post(
+    "/createPosts", status_code=status.HTTP_201_CREATED, response_model=schemas.Post
+)
+def create_posts(post: schemas.CreatePost, db: Session = Depends(get_db)):
+    new_post = models.Post(**post.dict())
+    db.add(new_post)
+    db.commit()
+    db.refresh(new_post)
+    return new_post
 
 
-@app.post("/createPosts", status_code=status.HTTP_201_CREATED)
-def create_posts(post: Post):
-    print(post.title, post.content, post.published)
-    cursor.execute(
-        """INSERT INTO posts (title, content, published) VALUES (%s, %s, %s) RETURNING *""",
-        (post.title, post.content, post.published),
-    )
-    new_post = cursor.fetchone()
-    conn.commit()
-    return {"data": new_post}
-
-
-def find_post(id: int):
-    for p in my_posts:
-        if p["id"] == id:
-            return p
-
-
-def find_index_post(id: int):
-    for i, p in enumerate(my_posts):
-        if p["id"] == id:
-            return i
-
-
-@app.get("/posts/{id}")
-def get_post(id: int):
-    cursor.execute("""SELECT * from posts WHERE id = %s""", (str(id)))
-    post = cursor.fetchone()
+@app.get("/posts/{id}", response_model=schemas.Post)
+def get_post(id: int, db: Session = Depends(get_db)):
+    post = db.query(models.Post).filter(models.Post.id == id).first()
     if not post:
         # response.status_code = 404
         raise HTTPException(status_code=404, detail=f"{id} was no found")
-    return {"data": post}
+    return post
 
 
-@app.put("/posts/{id}")
-def update_post(id: int, post: Post):
-    cursor.execute(
-        """UPDATE posts SET title = %s, content = %s, published = %s RETURNING *""",
-        (post.title, post.content, post.published),
-    )
-    post = cursor.fetchone()
-    conn.commit()
-    if not post:
+@app.put("/posts/{id}", response_model=schemas.Post)
+def update_post(id: int, post: schemas.CreatePost, db: Session = Depends(get_db)):
+    post_query = db.query(models.Post).filter(models.Post.id == id)
+    if post_query.first() == None:
         # response.status_code = 404
         raise HTTPException(status_code=404, detail=f"{id} was no found")
-    return {"data": post}
+    post_query.update(post.dict(), synchronize_session=False)
+    db.commit()
+    return post
 
 
 @app.delete("/posts/{id}", status_code=status.HTTP_204_NO_CONTENT)
-def delete_post(id: int):
-    cursor.execute("""DELETE from posts WHERE id = %s RETURNING *""", (str(id)))
-    post = cursor.fetchone()
-    conn.commit()
-    if not post:
+def delete_post(id: int, db: Session = Depends(get_db)):
+    post = db.query(models.Post).filter(models.Post.id == id)
+    if post.first() == None:
         # response.status_code = 404
         raise HTTPException(status_code=404, detail=f"{id} was no found")
+
+    post.delete(synchronize_session=False)
+    db.commit()
     return Response(status_code=204)
+
+
+@app.post("/users", status_code=status.HTTP_201_CREATED)
+def create_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
+
+    # hash the password - user.password
+    hashed_password = utils.hash(user.password)
+    user.password = hashed_password
+
+    new_user = models.User(**user.dict())
+    db.add(new_user)
+    db.commit()
+    db.refresh(new_user)
+
+    return new_user
---
+++ app/schemas.py
@@ -0,0 +1,25 @@
+from pydantic import BaseModel, EmailStr
+from datetime import datetime
+
+
+class PostBase(BaseModel):
+    title: str
+    content: str
+    published: bool = True
+
+
+class CreatePost(PostBase):
+    pass
+
+
+class Post(PostBase):
+    id: int
+    created_at: datetime
+
+    class Config:
+        orm_mode = True
+
+
+class UserCreate(BaseModel):
+    email: EmailStr
+    password: str
