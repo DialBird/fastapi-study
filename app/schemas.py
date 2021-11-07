eb84a580d59c6eea09f669b65e200675adb3f2e7
Keisuke Taniguchi <rememberthatk@icloud.com>
2021-11-07 13:03:15 +0900

    remove redundant UserOut

--- app/schemas.py
+++ app/schemas.py
@@ -41,15 +41,6 @@ class PostOut(BaseModel):
         orm_mode = True
 
 
-class UserOut(BaseModel):
-    id: int
-    email: EmailStr
-    created_at: datetime
-
-    class Config:
-        orm_mode = True
-
-
 class UserCreate(BaseModel):
     email: EmailStr
     password: str
